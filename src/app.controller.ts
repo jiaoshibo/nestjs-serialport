9412335242ed31db764b0f6293f2fde0144474f6
jiaoshibo <jiaoshibo@syngo.net>
2022-07-26 16:06:20 +0800

    init project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Param, Post, Body} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,25 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('getPortList')
+  getPortList(){
+    return this.appService.getPortList()
+  }
+  @Get('openPort/:path')
+  async connectPort(@Param('path') path:string){
+    return await this.appService.connectPort(path)
+  }
+  @Get('closePort')
+  closePort(){
+    return this.appService.closePort()
+  }
+  @Post('writePort')
+  async writePort(@Body() body:{data:Array<number>}){
+    return await this.appService.writeData(body.data)
+  }
+  @Get('readPort')
+  async readPort(){
+    return await this.appService.readData()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+
+import {SerialPort} from 'serialport';
 
 @Injectable()
 export class AppService {
+  port: SerialPort;
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getPortList(){
+    return await SerialPort.list()
+  }
+
+  connectPort(path:string){
+    let responseData:{
+      status:boolean;
+      code:number
+    } = {
+      status:false,
+      code:200
+    }
+    return new Promise((resolve, reject) => {
+      this.port = new SerialPort({path: path, baudRate: 9600}, (err) => {
+        if (err) {
+          console.log(`${path} 打开失败`);
+          responseData.code = 2001;
+          responseData.status = false;
+          reject(responseData)
+        }
+        responseData.code = 200;
+        responseData.status = true;
+        resolve(responseData)
+      })
+    })
+  }
+
+  closePort(){
+    this.port.close();
+    return true
+  }
+
+  writeData(data:Array<number>){
+    let uint8Array = new Uint8Array(data)
+    return new Promise((resolve,reject)=>{
+      this.port.write(uint8Array)
+      this.port.drain(err=>{
+        if(err){
+          console.log(err)
+          reject({
+            status:err,
+            code:2001
+          })
+        }
+        resolve({
+          status:'写入ok',
+          code:200
+        })
+      })
+    })
+  }
+
+  readData(){
+    return {
+      data:this.port.read(),
+      code:200
+    }
   }
 }
